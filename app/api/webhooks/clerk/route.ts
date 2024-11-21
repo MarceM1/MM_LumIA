bee1e228d1686f175e2e131ef52be93b1e19f8b5
Marcelo Melogno <marcelomelogno1986@gmail.com>
2024-11-21 03:27:00 -0300

    fix env variables

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -145,7 +145,7 @@ export async function POST(req: Request) {
   const svix_id = headerPayload.get("svix-id");
   const svix_timestamp = headerPayload.get("svix-timestamp");
   const svix_signature = headerPayload.get("svix-signature");
-
+  console.log("Headers recibidos:", headerPayload);
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
     return new Response("Error occured -- no svix headers", {
